ab7406da5696f6028bfec0d670b68dc810842d5e
NadimRai <rai_nadim@yahoo.com>
2017-09-14 21:40:39 +0800

    install yargs package

--- app.js
+++ app.js
@@ -4,5 +4,8 @@ request({
 	url: "https://www.google.com.hk/maps/api/geocode/json?address=%20hongkong",
 	json: true
 }, (error, response, body)=>{
-	console.log(body);
+	// console.log(JSON.stringify(response, undefined, 2));
+	console.log(`Address: ${body.results[0].formatted_address}`);
+	console.log(`Latitude: ${body.results[0].geometry.location.lat}`);
+	console.log(`Longitude: ${body.results[0].geometry.location.lng}`);
 });
